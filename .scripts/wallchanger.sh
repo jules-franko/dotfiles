bcc76fca5745a04faa40f34348f1c47228117403
jules-franko <julesfranko@proton.me>
2025-06-03 20:00:47 -0600

    scripts, ranger

--- .scripts/wallchanger.sh
+++ .scripts/wallchanger.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#sel="$(find $HOME/Pictures/Wallpapers | sort -f | dmenu -nb "#222222" -sb "#303030" -l 15 -fn terminus -p "Choose Wallpaper")"
+#sel="$(find $HOME/Pictures/Wallpapers | sort -f | dmenu -nb "#222222" -sb "#303030" -l 15 -fn terminus -p "Choose Wallpaper")" || exit 0
 sel=$(nsxiv -bort $HOME/Pictures/Wallpapers)
 wal -i "$sel"
